adc8c69e8a8aa02f972f76308e9a306bb298f301
enemiasufl <58480376+enemiasufl@users.noreply.github.com>
2021-10-26 12:12:50 -0400

    Update: shows weather data

--- project/src/App.js
+++ project/src/App.js
@@ -6,18 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
@@ -25,13 +13,22 @@ function App() {
 
 export default App;
 
+var place = "gainesville";
 
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 
-/*$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=miami&units=imperial&appid=55eedb3610cdff0867bc0990602170eb", function(data){
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" +place+ "&units=imperial&appid=55eedb3610cdff0867bc0990602170eb", function(data){
   console.log(data);
+
+  var icon = "http://api.openweathermap.org/img/w/" + data.weather[0].icon +".png";
+  var temp =data.main.temp + "°F";
+  var weather = data.weather[0].main;
+
+  $(".icon").attr("src", icon);
+  $(".temp").append(temp);
+  $(".weather").append(weather);
 }
-);*/
+);
 $.getJSON("http://dev.virtualearth.net/REST/v1/Routes?wayPoint.1=Miami,FL&Waypoint.2=Gainesville,FL&maxSolutions=4&distanceUnit=km&key=AixbZsh0iF2Uvsmk5iXVAdlztlQTsIU0lJntJCtS2mYY8OexSyDS3Vm8WOoKY9vm", function(data){
   console.log(data);
 }
