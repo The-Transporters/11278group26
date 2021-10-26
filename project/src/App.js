fd374d787b6a3304dee181ab895c0cc8fb0fd38a
enemiasufl <eitzep@ufl.edu>
2021-10-25 22:01:52 -0400

    Removed comments

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,1 @@
-
-
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-
-/*$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=miami&units=imperial&appid=55eedb3610cdff0867bc0990602170eb", function(data){
-  console.log(data);
-}
-);*/
-//$.getJSON("http://dev.virtualearth.net/REST/v1/Routes?wayPoint.1=Miami,FL&Waypoint.2=Gainesville,FL&maxSolutions=4&distanceUnit=km&key=AixbZsh0iF2Uvsmk5iXVAdlztlQTsIU0lJntJCtS2mYY8OexSyDS3Vm8WOoKY9vm", function(data){
-//  console.log(data);
-//}
-//);
