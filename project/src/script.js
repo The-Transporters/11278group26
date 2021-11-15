32f0363d3bcb8ab40679d399c40f6a989834605f
icraig1022 <iancraig1022@gmail.com>
2021-11-14 23:26:27 -0500

    implemented app using React components

--- project/src/script.js
+++
@@ -1,6 +0,0 @@
-import $ from 'jquery';
-
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=miami&units=imperial&appid=55eedb3610cdff0867bc0990602170eb", function(data){
-  console.log(data);
-}
-);
